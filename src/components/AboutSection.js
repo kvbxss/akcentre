c57e2851193c78099bbe6b188a7f733b95bb2a72
kvbxss <kubadownar444@gmail.com>
2022-03-27 19:53:32 +0200

    enters

---
+++ src/components/AboutSection.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components/native';
+
+
+function AboutSection({ lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  buttonLabel,
+  primary,
+  dark,
+  dark2,
+  img,
+  alt,
+  Link}) {
+  (
+    <AboutSection>
+        <AboutContainer id='about'>
+            <AboutH1>Trochę o AKcentrum</AboutH1>
+            <AboutWrapper></AboutWrapper>
+        </AboutContainer>
+    </AboutSection>
+  )
+}
+
+export const Container = styled.View`
+  
+`;
+
+
