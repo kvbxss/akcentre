eb6383ebf4dd7272b19b530e339ac2c35965093b
kvbxss <kubadownar444@gmail.com>
2022-05-25 20:12:04 +0200

    CARDS INFO

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,46 +6,157 @@ import Icon3 from "../assets/images/svg-7.svg";
 import Icon4 from "../assets/images/svg-8.svg";
 import Icon5 from "../assets/images/svg-9.svg";
 import Icon6 from "../assets/images/svg-10.svg";
+import Swal from "sweetalert2/src/sweetalert2.js";
+import withReactContent from "sweetalert2-react-content";
+import '@sweetalert2/theme-dark/dark.css';
 
+const MySwal=withReactContent(Swal);
+
+const launchAlert1= () => {
+  MySwal.fire({          
+   title: <p>Gym Kids!
+     <p></p>
+     Ucząc elementów gimnastycznych rozkładamy je na części pierwsze, 
+     dostosowując ćwiczenie do możliwości danej osoby, 
+     a na koniec uzupełniamy to własnym doświadczeniem.</p>,
+   footer: 'Copyright 2022',
+   confirmButtonText: <h3>Okej!</h3>,
+   confirmButtonColor: '#7189ff',
+   width: 600,
+   padding: '3em',
+   color: '#7189ff',
+   iconHtml: <img src={Icon1} alt="xd" height={150} width= {400}/>,
+ })
+}
+
+const launchAlert2= () => {
+  MySwal.fire({          
+   title: <p>Treningi Personalne
+     <p></p>
+     Dobrze dobrany plan treningowy pozwoli Ci dojść do wymarzonej figury.<p></p> 
+     Pamiętaj, że musi być dobrany idealny pod Ciebie, 
+     nie ściągnięty z internetu, instagrama czy podpatrzony od koleżanki.<p></p>
+     U nas znajdziecie specjalistów, którzy rozpiszą wam idealny plan treningowy, 
+     dobrany pod wasze cele😎</p>,
+   footer: 'Copyright 2022',
+   confirmButtonText: <h3>Okej!</h3>,
+   confirmButtonColor: '#7189ff',
+   width: 600,
+   padding: '3em',
+   color: '#7189ff',
+   iconHtml: <img src={Icon2} alt="xd" height={150} width= {400}/>,
+ })
+}
+
+const launchAlert3= () => {
+  MySwal.fire({          
+   title: <p>Treningi Medyczne
+     <p></p>
+     Holistycznie (ostatnio dość modne słowo) podchodzeimy do pacjenta, 
+     dobierając jak najlepsze narzędzia pracy do danego celu.<p></p>
+     Praca w studiu, gdzie możemy trenować 1:1 umożliwia realizację planu,
+    skupiając się wyłącznie na podopiecznym.
+</p>,
+   footer: 'Copyright 2022',
+   confirmButtonText: <h3>Okej!</h3>,
+   confirmButtonColor: '#7189ff',
+   width: 600,
+   padding: '3em',
+   color: '#7189ff',
+   iconHtml: <img src={Icon3} alt="xd" height={150} width= {400}/>,
+ })
+}
+
+const launchAlert4= () => {
+  MySwal.fire({          
+   title: <p>Zajęcia Grupowe
+     <p></p>
+     Czyli 'Zdrowy Kręgosłup' 
+     'Interwały' oraz wcześniej wspomniane już 'Gym Kids'</p>,
+   footer: 'Copyright 2022',
+   confirmButtonText: <h3>Okej!</h3>,
+   confirmButtonColor: '#7189ff',
+   width: 600,
+   padding: '3em',
+   color: '#7189ff',
+   iconHtml: <img src={Icon4} alt="xd" height={150} width= {400}/>,
+ })
+}
+
+const launchAlert5= () => {
+  MySwal.fire({          
+   title: <p>Diagnostyka
+     <p></p>
+     Najpierw rozmowa, ustalenie głównego celu, przeprowadzenie wywiadu odnośnie chorób, przeciwwskazań, kontuzji etc. <p></p>
+📈 Diagnostyka funkcjonalna. Warto zabrać ze sobą strój sportowy, aby dokładnie sprawdzić aparat ruchu i jego ruchomości. Tutaj bardzo często wkrada się konsternacja <p></p>
+  🤯Twoim celem była redukcja, a okazuje się, że trzeba się najpierw naprawić.
+🗣 Znowu rozmowa, ustalamy plan na nowo, teraz ten główny, namacalny. Ten, który przyniesie najlepsze korzyści dla Twojego zdrowia 💊 i sylwetki🧘🏻‍♂️
+<p></p>
+🏋🏼‍♂️ Trenujemy. A w zasadzie przepracowujemy przykładowy plan treningowy, uwzględniając wyżej wymienione kontuzje. <p></p>
+To wszystko w zaledwie godzinnym, darmowym spotkaniu!</p>,
+   footer: 'Copyright 2022',
+   confirmButtonText: <h3>Okej!</h3>,
+   confirmButtonColor: '#7189ff',
+   width: 600,
+   padding: '3em',
+   color: '#7189ff',
+   iconHtml: <img src={Icon5} alt="xd" height={150} width= {400}/>,
+ })
+}
+
+const launchAlert6= () => {
+  MySwal.fire({          
+   title: <p>Neurotreningi
+     <p></p>
+     Przywracamy poprzez ćwiczenia aparat ruchu do prawidłowych funkcji!</p>,
+   footer: 'Copyright 2022',
+   confirmButtonText: <h3>Okej!</h3>,
+   confirmButtonColor: '#7189ff',
+   width: 600,
+   padding: '3em',
+   color: '#7189ff',
+   iconHtml: <img src={Icon6} alt="xd" height={150} width= {400}/>,
+ })
+}
 
 const Services = () => {
   return (
-    <ServicesContainer id="services">
+    <ServicesContainer id="services" >
       <ServicesH1>Nasze Usługi</ServicesH1>
       <ServicesWrapper>
-      <ServicesCard>
+      <ServicesCard onClick={launchAlert1}>
           <ServicesIcon src={Icon1} />
           <ServicesH2>Gym Kids</ServicesH2>
           <ServicesP>
            Zajęcia gimnastyczne dla dzieci 🤸🏻‍♀️
           </ServicesP>
         </ServicesCard>
-        <ServicesCard>
+        <ServicesCard onClick={launchAlert2}>
           <ServicesIcon src={Icon2} />
           <ServicesH2>Treningi Personalne</ServicesH2>
           <ServicesP>
            Po prostu #nomercy. 
           </ServicesP>
         </ServicesCard>
-        <ServicesCard>
+        <ServicesCard onClick={launchAlert3}>
           <ServicesIcon src={Icon3} />
           <ServicesH2>Treningi Medyczne</ServicesH2>
           <ServicesP></ServicesP>
         </ServicesCard>
-        <ServicesCard>
+        <ServicesCard onClick={launchAlert4}>
           <ServicesIcon src={Icon4} />
           <ServicesH2>Zajęcia Grupowe</ServicesH2>
           <ServicesP>
           </ServicesP>
         </ServicesCard>
-        <ServicesCard>
+        <ServicesCard onClick={launchAlert5}>
           <ServicesIcon src={Icon5} />
           <ServicesH2>Diagnostyka</ServicesH2>
           <ServicesP>
           i konsultacje fizjoterapeutyczne
           </ServicesP>
         </ServicesCard>
-        <ServicesCard>
+        <ServicesCard onClick={launchAlert6}>
           <ServicesIcon src={Icon6} />
           <ServicesH2>Neurotreningi</ServicesH2>
           <ServicesP>
